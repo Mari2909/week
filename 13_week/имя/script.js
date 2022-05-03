85f39a950c05218c4e94b28cde96bbbb6b5d3774
Mari2909 <zmariy29@gmail.com>
2022-05-03 18:10:12 +0300

    added files

---
+++ 13_week/имя/script.js
@@ -0,0 +1,7 @@
+function showMessage() { 
+
+    let name = prompt("Как Вас зовут?");
+    
+    alert(`Привет, ${name}!`)
+}
+
---
+++ 13_week/калькулятор/script.js
@@ -0,0 +1,33 @@
+function showResultSum() { 
+
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+
+    alert(+a + +b);
+}
+
+
+function showResultSubtraction() { 
+
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+
+    alert(a-b);
+}
+
+function showResultDivision() { 
+
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+
+    alert(a/b);
+}
+
+function showResultMultiplication() { 
+
+    let a = prompt("Первое число?");
+    let b = prompt("Второе число?");
+
+    alert(a*b);
+}
+
